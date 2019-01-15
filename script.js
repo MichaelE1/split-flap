fb230da6ddae16a6ebde6c7bcd45ba91a2be67b3
MichaelE1 <mickel13@gmail.com>
2019-01-15 11:37:16 +1000

    add random word

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 const chars = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "(", ")", "^", "-", "_", "+", "&", "=", ";", ":", "*", "'", '"', ",", ".", "<", ">", "/", "\\", "?", "%"];
 
-let letters = document.querySelectorAll('.letter');
-let message = 'PARTY PEOPLE';
+const letters = document.querySelectorAll('.letter');
 
-// Turn the message into an associative array!
-let splitMessage = message.toUpperCase().split('');
-let messageArray = [];
-for (const [index, value] of splitMessage.entries()) {
-  messageArray[index] = value;
-};
+main();
 
-// For each letter in the array, display it progressively 
-for (let [key, value] of Object.entries(messageArray)) {
-  letterLoop(key, value, 0);
-};
+setInterval(() => {
+  main()
+}, 7000);
+
+function main() {
+  // Get a random word
+  let word = words[(Math.floor((Math.random() * words.length)))];
+
+  // Attempt to space word in middle of row
+  const spacesToAdd = 8 - Math.ceil(word.length / 2) 
+  for (i = 1; i < spacesToAdd; i++) {
+   word = ' ' + word;
+  };
+
+  // Fill the remaining spaces with blanks
+  while (word.length < letters.length) {
+   word += ' ';
+  };
+
+  // Turn the word into an associative array!
+  const splitWord = word.toUpperCase().split('');
+  const wordArray = [];
+  for (const [index, value] of splitWord.entries()) {
+   wordArray[index] = value;
+  };
+
+  // For each letter in the array, display it progressively 
+  for (let [key, value] of Object.entries(wordArray)) {
+    letterLoop(key, value, 0);
+  };
+}
 
 function letterLoop(key, value, index) {
-  // key = index of messageArray, value = letter, index = index of chars
+  // key = index of wordArray, value = letter, index = index of chars
   setTimeout(() => { 
     letters[key].innerHTML = chars[index];
     index++;
